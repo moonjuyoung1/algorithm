5f40ee72a02520a83c91502918e1f50b793ebddc
moonjuyoung1 <77542039+moonjuyoung1@users.noreply.github.com>
2021-10-12 21:22:17 +0900

    [modify] 알고리즘 2일차

--- day2.js
+++ day2.js
@@ -1,22 +1,10 @@
-const getLengthOfStr = (str) => {
-  if (str.indexOf(str[0], 1)) {
-    const number = str.indexOf(str[0], 1);
-    if (number === -1) {
-      return 0;
-    } else {
-      return number;
-    }
-  } else {
-    for (let i = 0; i < str.length; i++) {
-      for (let v = 1; v < str.length; i++) {
-        if (str[i] === str[v]) {
-          if (i + 1 > str.length - v) {
-            return i + 1;
-          } else {
-            return str.length - v;
-          }
-        }
-      }
+// 숫자로 이루어진 배열인 nums를 인자로 전달합니다. 숫자중에서 과반수(majority, more than a half)가 넘은 숫자를 반환해주세요.
+
+function moreThanHalf(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let count = nums.filter((element) => element === nums[i]).length;
+    if (count > nums.length / 2) {
+      return nums[i];
     }
   }
-};
+}
