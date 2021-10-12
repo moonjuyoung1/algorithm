10e3c78d7fc3695dd1ded38f18e035acf743cefc
moonjuyoung1 <77542039+moonjuyoung1@users.noreply.github.com>
2021-10-12 21:23:46 +0900

    [modify] 알고리즘 1일차

--- day1.js
+++ day1.js
@@ -1,9 +1,15 @@
-function number() {
-  let arr = [];
-  for (i = 1; i < 51; i++) {
-    if (i % 2 === 0) {
-      arr.push(i);
+// twoSum함수에 숫자배열과 '특정 수'를 인자로 넘기면, 더해서 '특정 수'가 나오는 index를 배열에 담아 return해 주세요.
+
+// nums: 숫자 배열
+// target: 두 수를 더해서 나올 수 있는 합계
+// return: 두 수의 index를 가진 숫자 배열
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let v = i + 1; v < nums.length; v++) {
+      if (nums[i] + nums[v] === target) {
+        return [i, v];
+      }
     }
   }
-  return arr;
-}
+};
