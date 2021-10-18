fca00c59864d9fa2a516ec6e4f8faeb756385b4b
moonjuyoung1 <77542039+moonjuyoung1@users.noreply.github.com>
2021-10-18 21:39:44 +0900

    [modify] day3

--- day3.js
+++ day3.js
@@ -13,3 +13,16 @@ process.stdin.on("data", (data) => {
     console.log(star.repeat(a));
   }
 });
+
+// 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다.
+// 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+function solution(x, n) {
+  var answer = [];
+  const initial = x;
+  for (let i = 0; i < n; i++) {
+    answer.push(x);
+    x = x + initial;
+  }
+  return answer;
+}
